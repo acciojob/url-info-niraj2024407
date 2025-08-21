c41bd98849a2ff580bfb752cd77cb733e395080a
niraj2024407 <neerajkumar04051998@gmail.com>
2025-08-22 05:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+ function showUrlLength() {
+            const currentUrl = window.location.href;
+            const urlLength = currentUrl.length;
+            alert(`The length of the URL is: ${urlLength}`);
+        }
